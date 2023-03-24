0c5f3c1e8bceb4b9d520501c79f3a1c21f168c32
GulshirIslam <128343172+GulshirIslam@users.noreply.github.com>
2023-03-24 10:16:47 +0530

    Python sample program to submit signals (#7)
    chore: Python sample program to submit signals

---
+++ python/app/communication.py
@@ -0,0 +1,59 @@
+import requests
+
+from utils.auth import get_basic_auth_header
+from utils.common import mergeDict
+from utils.constants import BASE_URL, BASIC_AUTH_API
+
+
+class Communication:
+
+    def __init__(self):
+        self.access_token = None
+
+    def get_auth_token(self):
+        # can be improved with using access token expiry
+        if self.access_token is None:
+            url = BASE_URL+BASIC_AUTH_API
+            headers = {
+                "Authorization": get_basic_auth_header()
+            }
+            response = requests.post(url, data=None, headers=headers)
+            if response.status_code == 200:
+                auth_response = response.json()
+                self.access_token = auth_response['accessToken']
+        return self.access_token
+
+
+    def get(self, url, body=None, extra_headers=None):
+        headers = {"Authorization": "Bearer {token}".format(token=Communication.get_auth_token(self))}
+        if extra_headers:
+            headers = mergeDict(extra_headers, headers)
+
+        res = requests.get(url, params=body, headers=headers, timeout=60)
+        response_status_code = res.status_code
+        print(
+            "GET response Status Code {response_status_code}".format(
+                response_status_code=response_status_code
+            )
+        )
+        return res
+
+
+    def post(self, url, body=None, extra_headers=None):
+        headers = {"Authorization": "Bearer {token}".format(token=Communication.get_auth_token(self))}
+        if extra_headers:
+            headers = mergeDict(extra_headers, headers)
+
+        res = requests.post(
+            url,
+            data=body,
+            headers=headers,
+            timeout=60,
+        )
+        response_status_code = res.status_code
+        print(
+            "GET response Status Code {response_status_code}".format(
+                response_status_code=response_status_code
+            )
+        )
+        return res
---
+++ python/app/utils/common.py
@@ -0,0 +1,4 @@
+def mergeDict(dict1, dict2):
+    temp = dict1.copy()
+    temp.update(dict2)
+    return temp
