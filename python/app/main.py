dbbc5e4567b7494a443009b8e11054f93c3d146f
GulshirIslam <128343172+GulshirIslam@users.noreply.github.com>
2023-03-28 12:54:29 +0530

    fix:moved user creds read from config file (#9)

--- python/app/main.py
+++ python/app/main.py
@@ -1,7 +1,9 @@
 from utils.signals import submitSampleSignals
-
+from config import Config
 
 def main():
+    # Initializing config data
+    Config()
     submitSampleSignals()
     
 if __name__ == "__main__": 
